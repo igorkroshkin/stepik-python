ce80fe283ceaee79e23ed6967c391c08f7ba94ab
Igor Kroshkin <igor.kroshkin@gmail.com>
2024-08-05 11:35:38 +0300

    0508202401

--- ch.11/test.py
+++ ch.11/test.py
@@ -1,4 +1,16 @@
-str1 = input()
-str2 = input()
+list = input().split()
 
-print(str2.join(str1))
+list = [(item[1:] + item[0] + "ки") for item in list]
+
+print(*list, sep = " ")
+
+#for i in range(len(list)):
+#    item = list[i]
+#    swap.append(item[1:]+ item[0])
+
+#swap = [item += "ки" for item in swap]
+
+#for i in range(len(swap)):
+#    swap[i] += "ки"
+    
+#print(*swap, sep = " ") 
